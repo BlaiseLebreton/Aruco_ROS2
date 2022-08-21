51c92424c391f930aae7785fde2a4cf4c42530e7
Blaise LEBRETON <59114761+BlaiseLebreton@users.noreply.github.com>
2022-08-21 17:39:25 +0000

    Refactor

--- include/aruco_tracker.hpp
+++ include/aruco_tracker.hpp
@@ -7,9 +7,21 @@
 #include "sensor_msgs/msg/image.hpp"
 #include "cv_bridge/cv_bridge.h"
 #include "image_transport/image_transport.hpp"
+#include "tf2/convert.h"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2/LinearMath/Transform.h"
+#include "tf2_ros/buffer_interface.h"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "geometry_msgs/msg/pose_array.hpp"
+#include "geometry_msgs/msg/quaternion.hpp"
+#include "aruco_msgs/msg/aruco_marker_array.hpp"
 
 #include <opencv2/highgui.hpp>
 #include <opencv2/aruco.hpp>
+#include <opencv2/calib3d.hpp>
+#include "KFilter/include/KFilter.h"
 
 using namespace std::chrono_literals;
 
@@ -24,10 +36,18 @@ private:
     sensor_msgs::msg::CameraInfo::ConstSharedPtr info_msg
   );
 
+  aruco_msgs::msg::ArucoMarkerArray RunDetection(
+    sensor_msgs::msg::Image::ConstSharedPtr image_msg,
+    sensor_msgs::msg::CameraInfo::ConstSharedPtr info_msg
+  );
+
+  aruco_msgs::msg::ArucoMarkerArray RunFilter(aruco_msgs::msg::ArucoMarkerArray markers);
+
+  // ROS Pub/Sub
   image_transport::CameraSubscriber sub_image_;
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
-  size_t count_;
+  rclcpp::Publisher<aruco_msgs::msg::ArucoMarkerArray>::SharedPtr pub_aru_;
+  rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr pub_pos_;
+  std::shared_ptr<tf2_ros::TransformBroadcaster> tf_publisher_;
 
   // Parameters
   int dictionaryId;
--- src/aruco_tracker.cpp
+++ src/aruco_tracker.cpp
@@ -1,8 +1,10 @@
 #include "aruco_tracker.hpp"
 
 ArucoTracker::ArucoTracker() : Node("aruco_tracker") {
-  sub_image_ = image_transport::create_camera_subscription(this, "image", std::bind(&ArucoTracker::ImageCallback, this, std::placeholders::_1, std::placeholders::_2), "raw");
-  publisher_ = this->create_publisher<sensor_msgs::msg::Image>("detection", rclcpp::SystemDefaultsQoS());
+  tf_publisher_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
+  sub_image_    = image_transport::create_camera_subscription(this, "image", std::bind(&ArucoTracker::ImageCallback, this, std::placeholders::_1, std::placeholders::_2), "raw");
+  pub_pos_      = this->create_publisher<geometry_msgs::msg::PoseArray>("poses",     rclcpp::SystemDefaultsQoS());
+  pub_aru_      = this->create_publisher<aruco_msgs::msg::ArucoMarkerArray>("aruco",     rclcpp::SystemDefaultsQoS());
 
   dictionaryId = 0;
   estimatePose = false;
@@ -16,15 +18,30 @@ ArucoTracker::ArucoTracker() : Node("aruco_tracker") {
 void ArucoTracker::ImageCallback(
   sensor_msgs::msg::Image::ConstSharedPtr image_msg,
   sensor_msgs::msg::CameraInfo::ConstSharedPtr info_msg) {
+    aruco_msgs::msg::ArucoMarkerArray markers_detected = RunDetection(image_msg, info_msg);
+    aruco_msgs::msg::ArucoMarkerArray markers_filtered = RunFilter(markers_detected);
+    pub_aru_->publish(markers_filtered);
+}
 
-  cv::Mat image, imageCopy;
+aruco_msgs::msg::ArucoMarkerArray ArucoTracker::RunDetection(
+  sensor_msgs::msg::Image::ConstSharedPtr image_msg,
+  sensor_msgs::msg::CameraInfo::ConstSharedPtr info_msg) {
+
+  // Output
+  aruco_msgs::msg::ArucoMarkerArray aruco_markers;
+  aruco_markers.header.frame_id = image_msg->header.frame_id;
+
+  // Load sensor info
   cv::Mat camMatrix = cv::Mat(info_msg->k).reshape(1, 3);
   cv::Mat distCoeff = cv::Mat(info_msg->d).t();
   camMatrix.convertTo(camMatrix, CV_64F);
   distCoeff.convertTo(distCoeff, CV_64F);
 
+  // Load image
+  cv::Mat image;
   image = cv::imread("/workspaces/ros-ws/launch/aruco.png");
 
+  // Variables
   std::vector<int> ids;
   std::vector<std::vector<cv::Point2f>> corners, rejected;
   std::vector<cv::Vec3d> rvecs, tvecs;
@@ -32,21 +49,47 @@ void ArucoTracker::ImageCallback(
   // Detect markers
   cv::aruco::detectMarkers(image, dictionary, corners, ids, detectorParams, rejected);
 
+  // Resize
+  aruco_markers.markers.resize(ids.size());
+
+  // Debug pose array
+  geometry_msgs::msg::PoseArray pose_array;
+  pose_array.header = aruco_markers.header;
+
   // Estimate their position
   if (ids.size() > 0) {
     cv::aruco::estimatePoseSingleMarkers(corners, markerLength, camMatrix, distCoeff, rvecs, tvecs);
-  }
-
-  // Draw on output image
-  image.copyTo(imageCopy);
-  if (ids.size() > 0) {
-    cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
-
-    // Draw axis
     for (unsigned int i = 0; i < ids.size(); i++) {
-      cv::aruco::drawAxis(imageCopy, camMatrix, distCoeff, rvecs[i], tvecs[i], markerLength * 2.5f);
+
+      // Convert rvecs to quaternions
+      cv::Mat R;
+      tf2::Quaternion quat_tf;
+      cv::Rodrigues(rvecs[i], R);
+      tf2::Matrix3x3 tf2_rot(R.at<double>(0, 0), R.at<double>(0, 1), R.at<double>(0, 2),
+                             R.at<double>(1, 0), R.at<double>(1, 1), R.at<double>(1, 2),
+                             R.at<double>(2, 0), R.at<double>(2, 1), R.at<double>(2, 2));
+      tf2::Transform tf2_transform(tf2_rot, tf2::Vector3());
+
+      // Fill data
+      aruco_markers.markers[i].id                 = ids[i];
+      aruco_markers.markers[i].pose.position.x    = tvecs[i][0];
+      aruco_markers.markers[i].pose.position.y    = tvecs[i][1];
+      aruco_markers.markers[i].pose.position.z    = tvecs[i][2];
+      aruco_markers.markers[i].pose.orientation.x = tf2_transform.getRotation().getX();
+      aruco_markers.markers[i].pose.orientation.y = tf2_transform.getRotation().getY();
+      aruco_markers.markers[i].pose.orientation.z = tf2_transform.getRotation().getZ();
+      aruco_markers.markers[i].pose.orientation.w = tf2_transform.getRotation().getW();
+
+      // Debug pose array
+      pose_array.poses.push_back(aruco_markers.markers[i].pose);
+
     }
+    pub_pos_->publish(pose_array);
   }
-  sensor_msgs::msg::Image::SharedPtr msg = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", imageCopy).toImageMsg();
-  publisher_->publish(*msg.get());
+
+  return aruco_markers;
+}
+
+aruco_msgs::msg::ArucoMarkerArray ArucoTracker::RunFilter(aruco_msgs::msg::ArucoMarkerArray markers) {
+  return markers;
 }
